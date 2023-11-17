d90d55ae0c25a890ddee7f788a932fd24bfc54b5
Rbsilva2023 <robsonchristopher2022@gmail.com>
2023-11-16 23:43:03 -0300

    buscador funcionando "morango"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 function procuraSobremesa(sobremesas, valorDoInput) {
-  /* Desenvolva sua lógica a partir aqui */
+  const resultados = sobremesas.filter((sobremesa) =>
+   sobremesa.nome.toLowerCase().includes(valorDoInput.toLowerCase()) 
+  );
 
-  return; /* É necessário retornar uma lista de produtos */
+  return resultados
 }
+
+procuraSobremesa();
